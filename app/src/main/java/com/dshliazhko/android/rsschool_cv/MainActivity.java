9030615b3c75abaa4cb1c11310b8b3aa7f1f2727
DimaShliazhko <dshliazhko@gmail.com>
2021-04-22 20:13:49 +0300

    другая ветка

--- app/src/main/java/com/dshliazhko/android/rsschool_cv/MainActivity.java
+++ app/src/main/java/com/dshliazhko/android/rsschool_cv/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 private TextView textView;
@@ -11,7 +12,7 @@ private TextView textView;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_main);
 
     }
 }
